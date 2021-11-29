c6e4192ac005f4a53bdd93e1ef75637f3a916e90
Ryan Kelley <kelleyr@duvalschools.org>
2021-11-29 14:30:16 -0500

    v0.3
    Started pygame.init(), setup color values, created window caption.

--- myFirstPyGame.py
+++ myFirstPyGame.py
@@ -1,1 +1,19 @@
-# My First PyGame, Ryan Kelley, 11/29/21 1:58pm, v0.0 
+# My First PyGame, Ryan Kelley, 11/29/21 2:28pm, v0.3
+
+import pygame, sys
+from pygame.locals import * 
+
+# Start PyGame
+pygame.init()
+
+# Setup our window. l
+windowSurface = pygame.display.set_mode((500, 400), 0, 32)
+pygame.display.set_caption('Hello, world!')
+
+# Setup Colors 
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
