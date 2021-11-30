aa2d79f69c992a13959f4ec330ed0386f2ef533f
Ryan Kelley <kelleyr@duvalschools.org>
2021-11-30 13:17:04 -0500

    v0.3
    Added RGB color values.

--- 7B/myFirstPygame.py
+++ 7B/myFirstPygame.py
@@ -1,5 +1,19 @@
-# My First PyGame, Ryan Kelley, 11/30/21, 12:12PM, v0.1
+# My First PyGame, Ryan Kelley, 11/30/21, 1:14PM, v0.3
 
 import pygame, sys 
 from pygame.locals import * 
 
+# Initialize PyGame 
+pygame.init()
+
+# Setup the Window to draw on. 
+windowSurface = pygame.display.set_mode((500, 400), 0, 32)
+pygame.display.set_caption('My First PyGame')
+
+# Setup color values. 
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+PUKEBROWN = (150, 150, 25)
