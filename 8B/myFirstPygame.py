313dfb7727e5f84697ce72050a26602b9c7f93d7
Ryan Kelley <kelleyr@duvalschools.org>
2021-11-30 14:46:20 -0500

    v0.5
    Added color values, created text rectangle in center of window.

--- 8B/myFirstPygame.py
+++ 8B/myFirstPygame.py
@@ -1,4 +1,31 @@
-# My First PyGame, Ryan Kelley, 11/30/21, 2:13PM, v0.1
+# My First PyGame, Ryan Kelley, 11/30/21, 2:45PM, v0.5
 
 import pygame, sys 
 from pygame.locals import * 
+
+# Start PyGame 
+pygame.init() 
+
+# Setup the game window.  
+windowSurface = pygame.display.set_mode((500, 400), 0, 32)
+pygame.display.set_caption('Hello, world!')
+
+# Setup color values. 
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+PUKEGREEN = (125, 255, 25)
+
+# Setup fonts. 
+basicFont = pygame.font.SysFont(None, 48)
+
+# Setup text.
+text = basicFont.render('Hello, world!', True, WHITE, BLUE)
+textRect = text.get_rect()
+textRect.centerx = windowSurface.get_rect().centerx
+textRect.centery = windowSurface.get_rect().centery
+
+
+
