5246632284d725e36a42adbc9c66054779d01382
Ryan Kelley <kelleyr@duvalschools.org>
2022-01-04 10:10:07 -0500

    v0.1
    Imported the pygame, system, and random modules.
    
    Setup PyGame and created mainClock variable.

--- collision_detection.py
+++ collision_detection.py
@@ -1,1 +1,8 @@
-# PyGame Collision Detection Practice, Ryan Kelley, January 04, 2022, 10:05am, v0.0
+# PyGame Collision Detection Practice, Ryan Kelley, January 04, 2022, 10:09am, v0.1
+
+import pygame, sys, random
+from pygame.locals import * 
+
+# Setup PyGame 
+pygame.init() 
+mainClock = pygame.time.Clock() 
